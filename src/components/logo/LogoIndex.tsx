384861545d7fa9eedf9b82423347e07a70e92488
a602988 <a680603@gmail.com>
2024-11-18 16:39:15 +0800

    1

--- src/components/logo/LogoIndex.tsx
+++ src/components/logo/LogoIndex.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import React from 'react';
 import { getWebData } from '@/services/getWebData';
 import { WebDataType } from '@/types/WebDataType';
+import styles from './LogoIndex.module.css';
 
 interface LogoData {
-  path: string;
+  content: string;
   width: number;
   height: number;
+  path: string;
 }
 
 interface LogoIndexProps {
@@ -17,17 +20,40 @@ interface LogoIndexProps {
 
 export default async function LogoIndex({
   className = '',
-  fallbackLogo = '/images/default-logo.png',
+  fallbackLogo = '/upload/images/common/logo.svg',
   linkToHome = true
 }: LogoIndexProps) {
     const webData: WebDataType = await getWebData();
     const logoData: LogoData | null = webData.system_logo ? JSON.parse(webData.system_logo)[0] : null;
 
-    const logoSrc = logoData?.path ? `/${logoData.path}` : fallbackLogo;
+    const logoSrc = logoData?.path || fallbackLogo;
     const logoWidth = logoData?.width || 234;
     const logoHeight = logoData?.height || 51;
 
-    const logoImage = (
+    const isSVG = logoSrc.toLowerCase().endsWith('.svg');
+
+    const logoImage = isSVG ? (
+        <div
+            aria-label={webData.site_title || "Company Logo"}
+            className={`${styles['logo-svg']} ${className}`}
+            style={{ width: logoWidth, height: logoHeight }}
+        >
+            <object
+                aria-label={webData.site_title || "Company Logo"}
+                data={logoSrc}
+                height={logoHeight}
+                type="image/svg+xml"
+                width={logoWidth}
+            >
+                <img
+                    alt={webData.site_title || "Company Logo"}
+                    height={logoHeight}
+                    src={logoSrc}
+                    width={logoWidth}
+                />
+            </object>
+        </div>
+    ) : (
         <Image
             alt={webData.site_title || "Company Logo"}
             className={className}
